7b1feb822c5fe0577a486f71d87b6dbffeb857d2
Sheuh Xiong <sheuhxiong@gmail.com>
2021-01-27 10:47:03 -0500

    Increased padding on a tag to fit li size

--- src/components/Navbar/UserDropDown.jsx
+++ src/components/Navbar/UserDropDown.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import "./styles/UserDropDown.css";
 
 function UserDropDown() {
+
   return (
-    <ul className="user-drop-down">
+    <ul className="ul-drop-down">
       <li>
         <a href="/">Profile</a>
       </li>
