1032e405b49c0034ba0e250550de33ee11bfe01c
Sheuh Xiong <sheuhxiong@gmail.com>
2021-01-27 13:33:14 -0500

    Added search-icon to search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,24 +1,33 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import "./styles/SearchBar.css";
+import searchIcon from "../../images/search-icon.png";
 
 function SearchBar() {
-
-  // eslint-disable-next-line no-unused-vars
   const [inputFieldCampsite, setInputFieldCampsite] = useState("");
+  const inputRef = useRef();
 
   const handleChange = (e) => setInputFieldCampsite(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputFieldCampsite);
+    inputRef.current.value = "";
+  }
 
   return (
     <div className="search-bar">
       <h2>Find your campsite</h2>
-      <div className="search-bar-lower">
+      <form className="search-bar-lower" onSubmit={handleSubmit}>
+        <div className="search-icon">
+          <img src={searchIcon} alt=""/>
+        </div>
         <input 
           type="text" 
           onChange={handleChange}
           placeholder="Enter a campsite name"
+          ref={inputRef}
         />
         <button>Search</button>
-      </div>
+      </form>
     </div>
   );
 }
