f593c503906e25ae0f6eef5ebd41eb4ae3d00126
Sheuh Xiong <sheuhxiong@gmail.com>
2021-02-22 10:33:48 -0500

    Added customom mini dropdown list in CampsiteHeader.jsx

--- src/components/campsite/SelectCampsite.jsx
+++ src/components/campsite/SelectCampsite.jsx
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import iconImg from "../../images/search-icon.png";
+import downArrow from "../../images/arrow-down.png";
+import OutsideClickHandler from 'react-outside-click-handler';
+import { Link } from 'react-router-dom';
 
 function SelectCampsite({ campsites, url, setURL }) {
+
+  const [isDropDownOpen, setDropDownOpen] = useState(false);
+  const openDropDown = () => setDropDownOpen(!isDropDownOpen);
+  const closeDropDown = () => setDropDownOpen(false);
+
   return (
     <form className="right-side-header">
-      <select name="select-campsite" className="select-campsite-header">
-        <option value="" defaultValue>-- Select another campsite --</option>
-        {
-          campsites.map((campsite) => {
-            return (
-              <option value={campsite.name} key={campsite.id}>{campsite.name}</option>
-            );
-          })
-        }
-      </select>
-      <div className="confirm-selected-campsite">
-        <img className="search-icon-header" src={iconImg} alt=""/>
+      <OutsideClickHandler onOutsideClick={closeDropDown}>
+        <div className="drop-down-header noselect" onClick={openDropDown}>
+          <span className="drop-down-span" >Select another campsite</span>
+          <img 
+            className={ (!isDropDownOpen) ? "down-arrow" : "up-arrow down-arrow"} 
+            src={downArrow} 
+            alt=""
+          />
+          <ul className={ (!isDropDownOpen) ? "drop-down-ul" : "drop-down-ul drop-down-ul-open" }>
+            {
+              campsites.map((campsite) => {
+                return (
+                  <Link className="drop-down-item" key={campsite.id} to={campsite.id}>
+                    <li>{campsite.name}</li>
+                  </Link>
+                );
+              })
+            }
+          </ul>
+        </div>
+      </OutsideClickHandler>
+      <div className="green-search-icon noselect">
+        <img src={iconImg} alt=""/>
       </div>
     </form>
   );
