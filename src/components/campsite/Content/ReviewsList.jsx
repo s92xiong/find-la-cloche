8b5a280a88ec35303ed8478b08cc1a6bfc1110d0
Sheuh Xiong <sheuhxiong@gmail.com>
2021-03-14 16:12:10 -0400

    Added white-space property with value pre-wrap to review-text-box (textarea) element in WriteReview.css

--- src/components/campsite/Content/ReviewsList.jsx
+++ src/components/campsite/Content/ReviewsList.jsx
@@ -6,11 +6,13 @@ function ReviewsList({ match }) {
   const [list, setList] = useState(null);
 
   const getReviews = async () => {
+    // Only retrieve data from Firestore once
     if (list) return;
+
+    // Retrieve reviews from Firestore, store data in "list" state
     const snapshot = await firestore.collection('campsites').doc(match.params.id).get();
     const data = snapshot.data();
     setList(data.reviews);
-    console.log(data.reviews);
   };
 
   useEffect(() => {
