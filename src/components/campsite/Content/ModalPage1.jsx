9579b1118544ac459bcb25c7143713ae9913556c
Sheuh Xiong <sheuhxiong@gmail.com>
2021-03-18 22:45:57 -0400

    Refaactored classnames in modal of WriteReview.jsx

--- src/components/campsite/Content/ModalPage1.jsx
+++ src/components/campsite/Content/ModalPage1.jsx
@@ -6,15 +6,21 @@ function ModalPage1({ setModalOpen, item, canContinue, handleSubmit }) {
     <div className="write-review-modal-1">
       <CloseModal setModalOpen={setModalOpen} />
       <h1 className="modal-title">{item.name}</h1>
-      <div className="modal-questions"></div>
+      <div className="modal-questions-container"></div>
       <div className="review-button-container">
-          <button 
-            className={ (canContinue) ? "review-button-next" : "review-button-invalid" }
-            onClick={handleSubmit}
-          >
-            Next
-          </button>
-        </div>
+        <button 
+          className="review-button"
+          onClick={handleSubmit}
+        >
+          Back
+        </button>
+        <button 
+          className={ (canContinue) ? "review-button" : "review-button-invalid" }
+          onClick={handleSubmit}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
