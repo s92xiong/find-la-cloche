5d608205d22ab59bbcf4960a8888d8fc6eb2434b
Sheuh Xiong <sheuhxiong@gmail.com>
2021-03-20 15:19:59 -0400

    added margin to bottom for Campsite.css, capitalized first letter of radioButton values, render radioButton values to ReviewsList.jsx

--- src/components/campsite/Content/ModalPage1.jsx
+++ src/components/campsite/Content/ModalPage1.jsx
@@ -12,71 +12,71 @@ function ModalPage1({ setModalOpen, item, canContinue, handleSubmit, setPageNum,
       <form className="modal-questions-form">
         <section className="modal-question">
           <p className="review-question">1. Spacing for tent:</p>
-          <input onChange={handleChange} type="radio" id="smallCampsite" name="campsiteSpacing" value="small" className="radio-button"/>
+          <input onChange={handleChange} type="radio" id="smallCampsite" name="campsiteSpacing" value="Small" className="radio-button"/>
           <label htmlFor="smallCampsite" className="radio-label">Small</label>
-          <input onChange={handleChange} type="radio" id="medCampsite" name="campsiteSpacing" value="medium" className="radio-button"/>
+          <input onChange={handleChange} type="radio" id="medCampsite" name="campsiteSpacing" value="Medium" className="radio-button"/>
           <label htmlFor="medCampsite" className="radio-label">Medium</label>
-          <input onChange={handleChange} type="radio" id="largeCampsite" name="campsiteSpacing" value="large" className="radio-button"/>
+          <input onChange={handleChange} type="radio" id="largeCampsite" name="campsiteSpacing" value="Large" className="radio-button"/>
           <label htmlFor="largeCampsite" className="radio-label">Large</label>
         </section>
 
         <section className="modal-question">
           <p className="review-question">2. Privacy:</p>
-          <input onChange={handleChange} type="radio" id="privacyPoor" name="privacy" value="poor" className="radio-button"/>
+          <input onChange={handleChange} type="radio" id="privacyPoor" name="privacy" value="Poor" className="radio-button"/>
           <label htmlFor="privacyPoor" className="radio-label">Poor</label>
-          <input onChange={handleChange} type="radio" id="privacySatisfactory" name="privacy" value="satisfactory" className="radio-button"/>
+          <input onChange={handleChange} type="radio" id="privacySatisfactory" name="privacy" value="Satisfactory" className="radio-button"/>
           <label htmlFor="privacySatisfactory" className="radio-label">Satisfactory</label>
-          <input onChange={handleChange} type="radio" id="privacyGood" name="privacy" value="good" className="radio-button"/>
+          <input onChange={handleChange} type="radio" id="privacyGood" name="privacy" value="Good" className="radio-button"/>
           <label htmlFor="privacyGood" className="radio-label">Good</label>
         </section>
 
         <section className="modal-question">
           <p className="review-question">3. Wind exposure:</p>
-          <input onChange={handleChange} type="radio" id="windLow" name="wind" value="low" className="radio-button"/>
+          <input onChange={handleChange} type="radio" id="windLow" name="wind" value="Low" className="radio-button"/>
           <label htmlFor="windLow" className="radio-label">Low</label>
-          <input onChange={handleChange} type="radio" id="windMed" name="wind" value="medium" className="radio-button"/>
+          <input onChange={handleChange} type="radio" id="windMed" name="wind" value="Medium" className="radio-button"/>
           <label htmlFor="windMed" className="radio-label">Normal</label>
-          <input onChange={handleChange} type="radio" id="windHigh" name="wind" value="high" className="radio-button"/>
+          <input onChange={handleChange} type="radio" id="windHigh" name="wind" value="High" className="radio-button"/>
           <label htmlFor="windHigh" className="radio-label">High</label>
         </section>
 
         <section className="modal-question">
           <p className="review-question">4. Thunderbox:</p>
-          <input onChange={handleChange} type="radio" id="thunderboxYes" name="thunderbox" value="yes" className="radio-button"/>
+          <input onChange={handleChange} type="radio" id="thunderboxYes" name="thunderbox" value="Yes" className="radio-button"/>
           <label htmlFor="thunderboxYes" className="radio-label">Yes</label>
-          <input onChange={handleChange} type="radio" id="thunderboxNo" name="thunderbox" value="no" className="radio-button"/>
+          <input onChange={handleChange} type="radio" id="thunderboxNo" name="thunderbox" value="No" className="radio-button"/>
           <label htmlFor="thunderboxNo" className="radio-label">No</label>
         </section>
 
         <section className="modal-question">
           <p className="review-question">5. Access to water:</p>
-          <input onChange={handleChange} type="radio" id="waterYes" name="water" value="yes" className="radio-button"/>
+          <input onChange={handleChange} type="radio" id="waterYes" name="water" value="Yes" className="radio-button"/>
           <label htmlFor="waterYes" className="radio-label">Yes</label>
-          <input onChange={handleChange} type="radio" id="waterNo" name="water" value="no" className="radio-button"/>
+          <input onChange={handleChange} type="radio" id="waterNo" name="water" value="No" className="radio-button"/>
           <label htmlFor="waterNo" className="radio-label">No</label>
         </section>
 
         <section className="modal-question">
           <p className="review-question">6. Firepit:</p>
-          <input onChange={handleChange} type="radio" id="firepitYes" name="firepit" value="yes" className="radio-button"/>
+          <input onChange={handleChange} type="radio" id="firepitYes" name="firepit" value="Yes" className="radio-button"/>
           <label htmlFor="firepitYes" className="radio-label">Yes</label>
-          <input onChange={handleChange} type="radio" id="firepitNo" name="firepit" value="no" className="radio-button"/>
+          <input onChange={handleChange} type="radio" id="firepitNo" name="firepit" value="No" className="radio-button"/>
           <label htmlFor="firepitNo" className="radio-label">No</label>
         </section>
 
         <section className="modal-question">
           <p className="review-question">7. Campsite seating:</p>
-          <input onChange={handleChange} type="radio" id="seatingYes" name="seating" value="yes" className="radio-button"/>
+          <input onChange={handleChange} type="radio" id="seatingYes" name="seating" value="Yes" className="radio-button"/>
           <label htmlFor="seatingYes" className="radio-label">Yes</label>
-          <input onChange={handleChange} type="radio" id="seatingNo" name="seating" value="no" className="radio-button"/>
+          <input onChange={handleChange} type="radio" id="seatingNo" name="seating" value="No" className="radio-button"/>
           <label htmlFor="seatingNo" className="radio-label">No</label>
         </section>
 
         <section className="modal-question">
           <p className="review-question">8. Hammock friendly:</p>
-          <input onChange={handleChange} type="radio" id="hammockYes" name="hammock" value="yes" className="radio-button"/>
+          <input onChange={handleChange} type="radio" id="hammockYes" name="hammock" value="Yes" className="radio-button"/>
           <label htmlFor="hammockYes" className="radio-label">Yes</label>
-          <input onChange={handleChange} type="radio" id="hammockNo" name="hammock" value="no" className="radio-button"/>
+          <input onChange={handleChange} type="radio" id="hammockNo" name="hammock" value="No" className="radio-button"/>
           <label htmlFor="hammockNo" className="radio-label">No</label>
         </section>
 
--- src/components/campsite/Content/ReviewsList.jsx
+++ src/components/campsite/Content/ReviewsList.jsx
@@ -13,26 +13,38 @@ function ReviewsList({ reviewsList }) {
           return (
             // Render photoURL, user name, rating, date, and text body
             <div className="review-container">
-              <div className="review-user-info">
-                <img src={review.photoURL} alt=""/>
-                <div className="user-review-date">
-                  <h4>{review.name}</h4>
-                  <div>
-                    <div className="user-star-container">
-                      {
-                        [...Array(5)].map((star, i) => {
-                          return (
-                            // If the index is less than the rating, then render a gold star
-                            <FaStar key={i} size={20} color={(i < rating) ? "#ffb400" : "#00000025"} />
-                          )
-                        })
-                      }
+              <div className="left-review-container">
+                <div className="review-user-info">
+                  <img src={review.photoURL} alt=""/>
+                  <div className="user-review-date">
+                    <h4>{review.name}</h4>
+                    <div>
+                      <div className="user-star-container">
+                        {
+                          [...Array(5)].map((star, i) => {
+                            return (
+                              // If the index is less than the rating, then render a gold star
+                              <FaStar key={i} size={20} color={(i < rating) ? "#ffb400" : "#00000025"} />
+                            )
+                          })
+                        }
+                      </div>
+                      <p>{review.date}</p>
                     </div>
-                    <p>{review.date}</p>
                   </div>
                 </div>
+                <p className="review-body">{review.text}</p>
               </div>
-              <p className="review-body">{review.text}</p>
+              <div className="right-review-container">
+                <span>Tent spacing: {review.questions.campsiteSpacing}</span>
+                <span>Firepit: {review.questions.firepit}</span>
+                <span>Hammock friendly: {review.questions.hammock}</span>
+                <span>Privacy: {review.questions.privacy}</span>
+                <span>Seating: {review.questions.seating}</span>
+                <span>Thunderbox: {review.questions.thunderbox}</span>
+                <span>Water access: {review.questions.water}</span>
+                <span>Wind protection: {review.questions.wind}</span>
+              </div>
             </div>
           );
         })
