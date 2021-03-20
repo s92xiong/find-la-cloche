22fdc69626153f87172214e0ddb6537bc26e6288
Sheuh Xiong <sheuhxiong@gmail.com>
2021-03-20 17:30:44 -0400

    Refactored xml in ReviewsList component

--- src/components/campsite/Content/ReviewsList.jsx
+++ src/components/campsite/Content/ReviewsList.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import { FaStar } from 'react-icons/fa';
+import { FaStar, FaCheck, FaTimes } from 'react-icons/fa';
 import "./styles/ReviewsList.css";
 
 function ReviewsList({ reviewsList }) {
+
+  const getYesOrNo = (value) => {
+    if (value === "Yes") {
+      return <FaCheck color="green" />
+    } else {
+      return <FaTimes color="red" />
+    }
+  }; 
 
   if (reviewsList.length === 0) return <></>;
   return (
@@ -13,38 +21,34 @@ function ReviewsList({ reviewsList }) {
           return (
             // Render photoURL, user name, rating, date, and text body
             <div className="review-container">
-              <div className="left-review-container">
-                <div className="review-user-info">
-                  <img src={review.photoURL} alt=""/>
-                  <div className="user-review-date">
-                    <h4>{review.name}</h4>
-                    <div>
-                      <div className="user-star-container">
-                        {
-                          [...Array(5)].map((star, i) => {
-                            return (
-                              // If the index is less than the rating, then render a gold star
-                              <FaStar key={i} size={20} color={(i < rating) ? "#ffb400" : "#00000025"} />
-                            )
-                          })
-                        }
-                      </div>
-                      <p>{review.date}</p>
+              <div className="review-user-info">
+                <img src={review.photoURL} alt=""/>
+                <div className="user-review-date">
+                  <h4>{review.name}</h4>
+                  <div>
+                    <div className="user-star-container">
+                      {
+                        [...Array(5)].map((star, i) => {
+                          return (
+                            // If the index is less than the rating, then render a gold star
+                            <FaStar key={i} size={20} color={(i < rating) ? "#ffb400" : "#00000025"} />
+                          )
+                        })
+                      }
                     </div>
+                    <p>{review.date}</p>
                   </div>
                 </div>
-                <p className="review-body">{review.text}</p>
               </div>
-              <div className="right-review-container">
-                <span>Tent spacing: {review.questions.campsiteSpacing}</span>
-                <span>Firepit: {review.questions.firepit}</span>
-                <span>Hammock friendly: {review.questions.hammock}</span>
+              <div className="questions-container">
+                <span>Firepit: {getYesOrNo(review.questions.firepit)}</span>
+                <span>Seating: {getYesOrNo(review.questions.seating)}</span>
+                <span>Hammock friendly: {getYesOrNo(review.questions.hammock)}</span>
+                <span>Thunderbox: {getYesOrNo(review.questions.thunderbox)}</span>
+                <span>Water access: {getYesOrNo(review.questions.water)}</span>
                 <span>Privacy: {review.questions.privacy}</span>
-                <span>Seating: {review.questions.seating}</span>
-                <span>Thunderbox: {review.questions.thunderbox}</span>
-                <span>Water access: {review.questions.water}</span>
-                <span>Wind protection: {review.questions.wind}</span>
               </div>
+              <p className="review-body">{review.text}</p>
             </div>
           );
         })
