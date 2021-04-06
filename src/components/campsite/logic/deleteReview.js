9b1ec8cb6d896f8194a7d6fefa9f39efe60e3670
Sheuh Xiong <sheuhxiong@gmail.com>
2021-04-06 01:18:25 -0400

    RWhen user deletes review, the review is deleted from the users collection document of user, as well as removed from the campsites document of campsite

--- src/components/campsite/logic/deleteReview.js
+++ src/components/campsite/logic/deleteReview.js
@@ -1,9 +1,10 @@
-import { firestore } from "../../../firebase";
+import { auth, firestore } from "../../../firebase";
 
 async function deleteReview(match, filteredReviews, item, setItem) {
   try {
     // Add filtered reviews to Firestore and update the current to state
     await firestore.collection("campsites").doc(match.params.id).update({ reviews: filteredReviews });
+    await firestore.collection("users").doc(auth.currentUser.uid).update({ reviews: filteredReviews });
     setItem({...item, reviews: filteredReviews});
   } catch (error) {
     console.error(error);
