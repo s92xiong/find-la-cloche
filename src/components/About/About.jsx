a18c1d3f9801534e2c955965ec570d9c747acc39
Sheuh Xiong <sheuhxiong@gmail.com>
2021-04-07 20:05:53 -0400

    Added gmail to About copmonent

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,6 +12,7 @@ function About() {
         <h2>About This App</h2>
         <p>Find LaCloche provides access to a database containing crowdsourced images and reviews for all 34 campsites along the <a target="blank" href={trailLink}>La Cloche Silhouette Trail</a>.</p>
         <p>Inspired by <a target="blank" href={allTrailsLink}>AllTrails</a>, this app serves as a guide to inform future backpackers of the topography and conditions of their reserved campsite.</p>
+        <p>For more information or additional inquiries, email us at findlacloche@gmail.com</p>
       </div>
     </div>
   );
